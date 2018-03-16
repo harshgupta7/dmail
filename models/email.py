98247f0b2bfa3d1be3bbb1fadec331c9620a9da5
harshgupta7 <hg@gatech.edu>
2018-03-16 17:25:49 -0400

    Models complete

---
+++ models/email.py
@@ -0,0 +1,46 @@
+from db import db
+
+class PendingEmails(db.Model):
+
+	__tablename__ = 'pendingemails'
+
+	id = db.Column(db.Integer, primary_key=True)
+	to = db.Column(db.String(80), nullable=False)
+	email_hash = db.Column(db.String(500), nullable=False)
+
+
+	def __init__(self, to, email_hash):
+
+		self.to = to
+		self.email_hash = email_hash
+
+
+
+	def save_to_db(self):
+
+		db.session.add(self)
+		db.session.commit()
+
+	def delete(self):
+
+		db.session.delete(self)
+		db.session.commit()
+
+
+	@staticmethod
+	def find_by_id(_id):
+
+		return PendingEmails.query.filter_by(id=_id).first()
+
+	@staticmethod
+	def find_by_to(to):
+
+		return PendingEmails.query.filter_by(to=to).all()
+
+	@staticmethod
+	def find_by_emailhash(email_hash):
+
+		return PendingEmails.query.filter_by(email_hash=email_hash).first()
+
+
+
